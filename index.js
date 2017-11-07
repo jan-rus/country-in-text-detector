d8fa6ea73f7a9537a8afbab73e3f15d35782b71f
Jan Rus <ing.janrus@gmail.com>
2017-11-07 23:11:16 +0100

    Added set of cities that could help with detection.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var io = require("./lib/io.js");
 var COUNTRIES = io.loadCoutries();
+var CITIES = io.loadCities();
 
 function detect(str) {
 	var result = [];
@@ -10,6 +11,20 @@ function detect(str) {
 			result.push({
 				iso3166: COUNTRIES[i]["country_iso_3166-1_alpha-2"],
 				name: COUNTRIES[i].country_name,
+				type: "country",
+				matches: matches
+			});
+		}
+	}
+
+	for(var i = 0; i < CITIES.length; i++){
+		var matches = str.match(CITIES[i].string_match);
+		if(!(typeof matches === "undefined" || matches === null)) {
+			matches = matches.map(function(m){return io.trim(m);});
+			result.push({
+				iso3166: CITIES[i]["country_iso_3166-1_alpha-2"],
+				name: CITIES[i].city_name,
+				type: "city",
 				matches: matches
 			});
 		}
