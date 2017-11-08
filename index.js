74e2facc6e00a5d60a98c096e62f7fbc401c9316
Jan Rus <ing.janrus@gmail.com>
2017-11-08 21:48:02 +0100

    special chars emoji detection optimization, handling prefixes

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ var io = require("./lib/io.js");
 var COUNTRIES = io.loadCoutries();
 var CITIES = io.loadCities();
 
+function deleteStrPrefixes(arr1, arr2){
+	var lastJ = 0;
+	for(var i = 0; i < arr1.length; i++){
+		for(var j = lastJ; j < arr2.length; j++){
+			if(arr1[i].startsWith(arr2[j])){
+				arr2.splice(j, 1);
+				lastJ = j;
+				i++;
+				break;
+			}
+			if(arr2[j].startsWith(arr1[i])){
+				arr1.splice(i, 1);
+				lastJ = j;
+				i++;
+				break;
+			}
+		}
+	}
+}
+
 function detect(str) {
+	var clean = " " + io.cleanFromSpecials(str);
 	var result = [];
 	for(var i = 0; i < COUNTRIES.length; i++){
-		var matches = str.match(COUNTRIES[i].string_match);
+		var matches = clean.match(COUNTRIES[i].string_match);
 		if(!(typeof matches === "undefined" || matches === null)) {
-			matches = matches.map(function(m){return io.trim(m);});
+			matches = matches.map(function(m){return m.substring(1);});
 			result.push({
 				iso3166: COUNTRIES[i]["country_iso_3166-1_alpha-2"],
 				name: COUNTRIES[i].country_name,
@@ -18,9 +39,9 @@ function detect(str) {
 	}
 
 	for(var i = 0; i < CITIES.length; i++){
-		var matches = str.match(CITIES[i].string_match);
+		var matches = clean.match(CITIES[i].string_match);
 		if(!(typeof matches === "undefined" || matches === null)) {
-			matches = matches.map(function(m){return io.trim(m);});
+			matches = matches.map(function(m){return m.substring(1);});
 			result.push({
 				iso3166: CITIES[i]["country_iso_3166-1_alpha-2"],
 				name: CITIES[i].city_name,
@@ -29,6 +50,20 @@ function detect(str) {
 			});
 		}
 	}
+
+	// fallback for prefixes
+	// if two results A and B have same name and same type
+	// and one of them (A) has match that is prefix of match of the other one (B)
+	// remove the shorter match (prefix)
+	for(var i = 0; i < result.length;i++){
+		for(var j = i+1; j < result.length;j++){
+			if(result[i].name == result[j].name && result[i].type == result[j].type){
+				deleteStrPrefixes(result[i].matches, result[j].matches);
+			}
+		}
+	}
+
+	result = result.filter(function(r){return r.matches.length;});
 
 	return result;
 }
